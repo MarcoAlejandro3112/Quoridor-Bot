ce8725fa13073b04634b3ce5e63871271715460a
Marco Altamirano <maaltamiranohiga@gmail.com>
2020-10-02 13:01:43 -0500

    -..

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let turno = 0
 let posiciones = [76,4,36,44]
-
+let winner = false;
 var nn = 81;
 var n = Number(Math.sqrt(nn));
 
@@ -71,34 +71,83 @@ class Stack {
 }
 
 
-function mapa1(){
-	/*MAPA 1*/
-	remove_edge(G,12,21)
-	remove_edge(G,13,22)
-	remove_edge(G,14,23)
-	remove_edge(G,21,22)
+function mapa1(on){
+	if(on){
+		remove_edge(G,12,21)
+		remove_edge(G,13,22)
+		remove_edge(G,14,23)
+		remove_edge(G,21,22)
 
-	remove_edge(G,57,66)
-	remove_edge(G,58,67)
-	remove_edge(G,59,68)
-	remove_edge(G,58,59)
+		remove_edge(G,57,66)
+		remove_edge(G,58,67)
+		remove_edge(G,59,68)
+		remove_edge(G,58,59)
 
-	remove_edge(G,28,29)
-	remove_edge(G,37,38)
-	remove_edge(G,46,47)
-	remove_edge(G,38,47)
+		remove_edge(G,28,29)
+		remove_edge(G,37,38)
+		remove_edge(G,46,47)
+		remove_edge(G,38,47)
 
-	remove_edge(G,33,34)
-	remove_edge(G,42,43)
-	remove_edge(G,51,52)
-	remove_edge(G,33,42)
-	/***********************/
+		remove_edge(G,33,34)
+		remove_edge(G,42,43)
+		remove_edge(G,51,52)
+		remove_edge(G,33,42)
+	}
+}
+
+function mapa2(on){
+	if(on){
+		remove_edge(G,2,11);
+		remove_edge(G,3,12);
+		remove_edge(G,5,14);
+		remove_edge(G,6,15);
+		remove_edge(G,13,22);
+
+		remove_edge(G,65,74);
+		remove_edge(G,66,75);
+		remove_edge(G,68,77);
+		remove_edge(G,69,78);
+		remove_edge(G,58,67);
+
+		remove_edge(G,19,20);
+		remove_edge(G,28,29);
+		remove_edge(G,46,47);
+		remove_edge(G,55,56);
+		remove_edge(G,29,30);
+		remove_edge(G,38,39);
+		remove_edge(G,47,48);
+
+		remove_edge(G,24,25);
+		remove_edge(G,33,34);
+		remove_edge(G,51,52);
+		remove_edge(G,60,61);
+		remove_edge(G,32,33);
+		remove_edge(G,41,42);
+		remove_edge(G,50,51);
+	}
 }
 
 var start = 76;
 var goal = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 var goal2 = [72,73,74,75,76,77,78,79,80];
-mapa1();
+var goal3 = [8,17,26,35,44,53,62,71,80]
+mapa1(false);
+mapa2(true);
+
+let stack = []
+let contador = -1
+function AI_Wilmar(G){
+    stack.push(posiciones[2])
+    if (posiciones[2] == 8 || posiciones[2] == 17 || posiciones[2] == 26 || posiciones[2] == 35 || posiciones[2] == 44 || posiciones[2] == 53 || posiciones[2] == 62 || posiciones[2] == 71 || posiciones[2] == 80) return posiciones[2]  
+    else {
+        contador++
+        if (G[posiciones[2]][G[posiciones[2]].length - 1] == posiciones[0] || G[posiciones[2]][G[posiciones[2]].length - 1] == posiciones[1] || G[posiciones[2]][G[posiciones[2]].length - 1] == posiciones[3]){
+        	a = G[posiciones[2]][G[posiciones[2]].length - 1]
+        	return G[a][G[posiciones[2]].length - 1]
+        }
+        else return G[posiciones[2]][G[posiciones[2]].length - 1]
+    }
+}
 
 function _dfs(G,root,goal){
 	stack = new Stack()
@@ -250,59 +299,83 @@ window.onload = function(){
 function conseguirKey(objeto){
 	let tecla = objeto.which
 	console.log(tecla)
-	desplazar(tecla)
+	if(!winner){
+		desplazar(tecla)
+	}
 }
 function cambiar_turno(turno){
-	if(turno == 0){
+	if(turno <= 2){
 		turno++;
 	} else {
 		turno = 0;
 	}
+	document.getElementById("caja_turno").innerHTML = turno;
 	return turno
 }
 function desplazar(tecla){
 	let ficha1,celda,id_1;
-	if(turno == 1){
+	if(turno == 0){
+		ficha1 = document.getElementById('j1')
+	} else if(turno == 1){
 		ficha1 = document.getElementById('j2')
 	} else if(turno == 2){
 		ficha1 = document.getElementById('j3')
 	} else {
 		ficha1 = document.getElementById('j4')
 	}
-	console.log("TURNO: " + turno);
+	
 	switch (tecla){
 		case 13:
 			if(turno == 0){
 				if (!goal.includes(posicion)) {
 	    			camino = a_star(G, posicion, goal, posiciones);
 	    			posicion = camino[1];
+	    			if(goal.includes(posicion)){
+	    				alert("GANO EL JUGADOR 0")
+	    				winner = true;
+	    			}
 	    			console.log(posiciones);
 	    			posiciones[turno] = posicion
 	    			console.log("Moverse a posicion: ", posicion);
 	    			id_1 = "celda-" + posicion;
 	    			center = document.createElement("center");
 	    			celda = document.getElementById(id_1)
+	    			center.appendChild(ficha1)
 	    			celda.appendChild(center)
-	    			ficha1 = document.getElementById('j1')
-	    			center.appendChild(ficha1);
-				};
+				} 
 				turno = cambiar_turno(turno);
 			} else if(turno == 1){
 				if(!goal2.includes(posiciones[1])){
 					camino = _dfs(G,posiciones[1],goal2)
+					if(goal2.includes(posicion)){
+						alert("GANO EL JUGADOR 1")
+						winner = true
+					}
 					console.log("CAMINO: " + camino)
 					posiciones[turno] = camino[1]
 					id_1 = "celda-" + camino[1];
 	    			center = document.createElement("center");
 	    			celda = document.getElementById(id_1)
-	    			celda.appendChild(center)
 	    			center.appendChild(ficha1);
+	    			celda.appendChild(center)
+				}
+				turno = cambiar_turno(turno)
+			} else if(turno == 2){
+				if(!goal3.includes(posiciones[2])){
+					camino = AI_Wilmar(G)
+					console.log("CAMINO: " + camino)
+					posiciones[turno] = camino
+					id_1 = "celda-" + camino;
+	    			center = document.createElement("center");
+	    			celda = document.getElementById(id_1)
+	    			center.appendChild(ficha1);
+	    			celda.appendChild(center)
 				}
 				turno = cambiar_turno(turno)
 			}
 		break;
 		case 97:
-			if(posiciones[turno] - 1 >= 0 && turno > 0){
+			if(posiciones[turno] - 1 >= 0 && turno > 2){
 				id_1 = "celda-" + (posiciones[turno] - 1)
 				center = document.createElement("center");
 				posiciones[turno] -=1
@@ -313,7 +386,7 @@ function desplazar(tecla){
 			}
 		break;
 		case 119:
-			if(posiciones[turno] - 9 >= 0 && turno > 0){ 
+			if(posiciones[turno] - 9 >= 0 && turno > 2){ 
 				id_1 = "celda-" + (posiciones[turno] - 9)
 				center = document.createElement("center");
 				posiciones[turno] -= 9
@@ -324,7 +397,7 @@ function desplazar(tecla){
 			}
 		break;
 		case 100:
-			if(posiciones[turno] + 1 <= 80 && turno > 0){
+			if(posiciones[turno] + 1 <= 80 && turno > 2){
 				id_1 = "celda-" + (posiciones[turno] + 1)
 				center = document.createElement("center");
 				posiciones[turno] += 1
@@ -335,7 +408,7 @@ function desplazar(tecla){
 			}
 		break;
 		case 115:
-			if(posiciones[turno] + 9 <= 80 && turno > 0){
+			if(posiciones[turno] + 9 <= 80 && turno > 2){
 				id_1 = "celda-" + (posiciones[turno] + 9)
 				center = document.createElement("center");
 				posiciones[turno] += 9
